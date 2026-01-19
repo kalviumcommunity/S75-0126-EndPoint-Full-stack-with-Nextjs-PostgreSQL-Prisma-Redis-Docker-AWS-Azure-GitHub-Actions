b62e894822755dd63c0082ac3a834d1924076478
jevinjosh <jevinjosh000@gmail.com>
2026-01-19 17:37:55 +0530

    completed database migration

--- digital/src/lib/prisma.ts
+++ digital/src/lib/prisma.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL,
+    log: ['query', 'info', 'warn', 'error'],
   });
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
